307fcf0592b2ad123def943064e22ff2f66c67a2
bkaneb <bryankaneb2@gmail.com>
2021-09-23 16:12:29 +0200

    modified of interface

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let numbersAll = document.querySelectorAll(".numbers");
 let inputAff = document.getElementById("inputaff");
-
+let rmzero = 0;
 
 for(let i = 0; i<numbersAll.length; i++)/*tant que le i ne correspond pas a celui du tableau on ne rentre pas dans la condition*/
 {
     numbersAll[i].addEventListener("click", function(event){
-    inputAff.value = inputAff.value + numbersAll[i].innerText;
+        if(rmzero == 0)
+        {
+            inputAff.value = "";
+            rmzero++;
+        }
+        inputAff.value = inputAff.value + numbersAll[i].innerText;
     })
 }
