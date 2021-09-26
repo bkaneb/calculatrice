2f5404bd7bdeb896dc516eb652234dbf8c824f4a
bkaneb <bryankaneb2@gmail.com>
2021-09-26 20:44:57 +0200

    Add all functionnality except %

--- script.js
+++ script.js
@@ -2,48 +2,139 @@ let numbersAll = document.querySelectorAll(".numbers");
 let inputAff = document.getElementById("inputaff");
 let historyAff = document.getElementById("historyaff");
 let rmzero = 0;
-let firstValueAddition = 0;
-let secondValueAddition = 0;
+let firstValue = 0;
+let secondValue = 0;
 let thirdvalue = 0;
-let iFplus = 0;
+let saisie = 0;
 let caseEgale;
 let recupOperator;
-/*function soustraction(){
-    if(iFplus == 1){
-        secondValueAddition = parseInt(inputAff.value);
-        inputAff.value = secondValueAddition;
-        thirdvalue = secondValueAddition - parseInt(historyAff.value);
+
+function soustraction(){
+    if(saisie == 0 && inputAff.value != "")
+    {
+        firstValue = parseFloat(inputAff.value);
+        inputAff.value = "";
+        historyAff.value = firstValue + "-";
+        rmzero++;
+        saisie++;
+    }
+    else if(saisie == 1){
+        secondValue = parseFloat(inputAff.value);
+        inputAff.value = secondValue;
+        thirdvalue = parseFloat(historyAff.value) - secondValue;
         if(rmzero == 1)
         {
-            historyAff.value = thirdvalue - "+";
+            historyAff.value = thirdvalue + "-";
         }else{
-            historyAff.value = firstValueAddition - "+";
+            historyAff.value = firstValue + "-";
             rmzero++;
         }
+        inputAff.value = "";
+        inputAff.placeholder = "0";
     }
-}*/
-function addition(){
-    if(iFplus == 0)//premiere saisie 
+}
+
+function multiplication(){
+    if(saisie == 0 && inputAff.value != "")
     {
-                firstValueAddition = parseInt(inputAff.value);
+        firstValue = parseFloat(inputAff.value);
+        inputAff.value = "";
+        historyAff.value = firstValue + "*";
+        rmzero++;
+        saisie++;
+    }
+    else if(saisie == 1){
+        secondValue = parseFloat(inputAff.value);
+        inputAff.value = secondValue;
+        thirdvalue = secondValue * parseFloat(historyAff.value);
+        if(rmzero == 1)
+        {
+            historyAff.value = thirdvalue + "*";
+        }else{
+            historyAff.value = firstValue + "*";
+            rmzero++;
+        }
+        inputAff.value = "";
+        inputAff.placeholder = "0";
+    }
+}
+
+function diviser(){
+    if(saisie == 0 && inputAff.value != "")
+    {
+        firstValue = parseFloat(inputAff.value);
+        inputAff.value = "";
+        historyAff.value = firstValue + "/";
+        rmzero++;
+        saisie++;
+    }
+    else if(saisie == 1){
+        secondValue = parseFloat(inputAff.value);//enlever les string en les convertisant en int
+        inputAff.value = secondValue;
+        thirdvalue =  parseFloat(historyAff.value) / secondValue;
+        if(rmzero == 1)
+        {
+            historyAff.value = thirdvalue + "/";
+        }else{
+            historyAff.value = firstValue + "/";
+            rmzero++;
+        }
+        inputAff.value = "";
+        inputAff.placeholder = "0";
+    }
+}
+
+
+function addition(){
+    if(saisie == 0 && inputAff.value != "")//premiere saisie 
+    {
+                firstValue = parseFloat(inputAff.value);
                 inputAff.value = "";
-                historyAff.value = firstValueAddition + "+";
+                historyAff.value = firstValue + "+";
                 rmzero++;
-                iFplus++;
+                saisie++;
 
     }else
-    if(iFplus == 1){//si ce n'est pas la premiere saisie dans inputaff
-        secondValueAddition = parseInt(inputAff.value);
-        inputAff.value = secondValueAddition;
-        thirdvalue = secondValueAddition + parseInt(historyAff.value);//on prend la derniere valeur calculer plus celle dans lecran principale
+    if(saisie == 1){//si ce n'est pas la premiere saisie dans inputaff
+        secondValue = parseFloat(inputAff.value);
+        inputAff.value = secondValue;
+        thirdvalue = secondValue + parseFloat(historyAff.value);//on prend la derniere valeur calculer plus celle dans lecran principale
         if(rmzero == 1)
         {
             historyAff.value = thirdvalue + "+";//autre que premiere saisie dans history
         }else{
-            historyAff.value = firstValueAddition + "+";
+            historyAff.value = firstValue + "+";
             rmzero++;
         }
+        inputAff.value = "";
+        inputAff.placeholder = "0";
     }
+}
+
+function clearCalcul(operator)
+{
+    recupOperator = historyAff.value;//trasnmet la donner du input history
+    caseEgale = recupOperator.substr(-1);//lis le dernier caractere qui est l'operateur
+    //termine le calcul en cours ex si on avait 6*6*6*1 on doit bien faire tous les * avant le +
+    if(saisie == 1){
+        if(caseEgale == "*" && inputAff.value != "" && operator !="*")
+    {
+        historyAff.value = parseInt(historyAff.value) * parseInt(inputAff.value) + operator;   
+        inputAff.value = "";
+    }else if(caseEgale == "/" && inputAff.value != "" && operator !="/")
+    {
+        historyAff.value = parseInt(historyAff.value) / parseInt(inputAff.value) + operator;   
+        inputAff.value = "";
+    }else if(caseEgale == "-" && inputAff.value != "" && operator !="-")
+    {
+        historyAff.value = parseInt(historyAff.value) - parseInt(inputAff.value) + operator;   
+        inputAff.value = "";
+    }else if(caseEgale == "+" && inputAff.value != "" && operator !="+")
+    {
+        historyAff.value = parseInt(historyAff.value) * parseInt(inputAff.value) + operator;   
+        inputAff.value = "";
+    }
+    } 
 }
 
 for(let i = 0; i<numbersAll.length; i++)/*tant que le i ne correspond pas a celui du tableau on ne rentre pas dans la condition*/
@@ -58,46 +149,55 @@ for(let i = 0; i<numbersAll.length; i++)/*tant que le i ne correspond pas a celu
    if(i == 2)//btn C
     {
         numbersAll[i].addEventListener("click", function(event){
-            inputAff.value = "";//mets la valeur a null pour pouvoir faire placeholder
-            inputAff.placeholder = "0";
+            //on efface tous donc on decremente aussi pour revenir a la premiere saisie
+            if(rmzero == 1 && saisie == 1)
+            {
+                rmzero--;
+                saisie--;   
+            }
+            inputAff.value = "";
+           // inputAff.placeholder = "0";
+            historyAff.value = "";
+            //historyAff.placeholder = "0";
         })
     }
     if(i == 1)// btn CE
     {
         numbersAll[i].addEventListener("click", function(event){
-            //on efface tous donc on decremente aussi pour revenir a la premiere saisie
-            rmzero--;
-            iFplus--;
-            inputAff.value = "";
-            inputAff.placeholder = "0";
-            historyAff.value = "";
-            historyAff.placeholder = "0";
+            
+            inputAff.value = "";//mets la valeur a null pour pouvoir faire placeholder
         })
     }
     if(i == 3)// btn -
     {
-        /*numbersAll[i].addEventListener("click", function(event){
-            
-                if(iFplus == 0)
-                {
-                    firstValueAddition = parseInt(inputAff.value);
-                    inputAff.value = "";
-                    historyAff.value = firstValueAddition + "-";
-                    rmzero++;
-                    iFplus++;
-    
-                }else
-                {
-                    soustraction();
-                }
-        
-        })      */
+        numbersAll[i].addEventListener("click", function(event){       
+           recupOperator = historyAff.value;//trasnmet la donner du input history
+            caseEgale = recupOperator.substr(-1);//lis le dernier caractere qui est l'operateur
+            if(caseEgale == "+" && inputAff.value != "")
+            {
+                historyAff.value = parseInt(inputAff.value) + parseInt(historyAff.value) + "-";   
+            }   
+            soustraction();        
+        })      
     }
     if(i==15)// btn +
     {
         numbersAll[i].addEventListener("click", function(event){
+            clearCalcul("+");
                 addition();
         })  
+    }
+    if(i==11)//btn *
+    {
+        numbersAll[i].addEventListener("click", function(event){
+            multiplication();
+    })  
+    }
+    if(i==7)//btn /
+    {
+        numbersAll[i].addEventListener("click", function(event){
+            diviser();
+    })  
     }
     if(i==18)//btn =
     {
@@ -105,20 +205,23 @@ for(let i = 0; i<numbersAll.length; i++)/*tant que le i ne correspond pas a celu
             recupOperator = historyAff.value;//trasnmet la donner du input history
             caseEgale = recupOperator.substr(-1);//lis le dernier caractere qui est l'operateur
             //verification des operateurs
-            if(caseEgale == "+"){
-                inputAff.value = parseInt(inputAff.value) + parseInt(historyAff.value);
-            }else if(caseEgale == "-"){
-                inputAff.value = parseInt(inputAff.value) + parseInt(historyAff.value);
-            }else if(caseEgale == "/"){
-                inputAff.value = parseInt(inputAff.value) + parseInt(historyAff.value);
-            }else if(caseEgale == "*"){
-                inputAff.value = parseInt(inputAff.value) + parseInt(historyAff.value);
-            }else if(caseEgale == "%"){
-                inputAff.value = parseInt(inputAff.value) + parseInt(historyAff.value);
+            if(inputAff.value != "")
+            {
+                if(caseEgale == "+"){
+                    inputAff.value = parseFloat(inputAff.value) + parseFloat(historyAff.value);
+                }else if(caseEgale == "-"){
+                    inputAff.value = parseFloat(historyAff.value) - parseFloat(inputAff.value);
+                }else if(caseEgale == "/"){
+                    inputAff.value = parseFloat(historyAff.value) / parseFloat(inputAff.value);
+                }else if(caseEgale == "*"){
+                    inputAff.value = parseFloat(inputAff.value) * parseFloat(historyAff.value);
+                }else if(caseEgale == "%"){
+                    inputAff.value = parseFloat(inputAff.value) + parseFloat(historyAff.value);
+                }
             }
             //
             historyAff.value = "";
-            iFplus--;
+            saisie--;
             rmzero--;
             console.log(caseEgale);
         })
